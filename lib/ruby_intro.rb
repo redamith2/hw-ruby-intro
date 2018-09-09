4737a80e757a7943a6eadd860e22e3ea386970d1
EC2 Default User <ec2-user@ip-172-31-29-83.us-east-2.compute.internal>
2018-09-09 01:01:09 +0000

    completed HW1

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,82 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  sum = 0
+  arr.each do |i|
+    sum+=i
+  end 
+  return sum
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  if arr.size == 0
+    return 0
+  end
+  if arr.size == 1
+    return arr[0]
+  end
+  arr = arr.sort
+  sum=0
+  sum = arr[-1] + arr[-2]
+  return sum
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  sum = Hash.new 
+  arr.each do |i|
+    if sum.has_key?(n-i)
+      return true
+    else
+      sum[i] = 1
+    end 
+  end
+  return false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  return "Hello, " + name
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  if s.size == 0
+    return false
+  end
+  
+  if s[0].match(/^[[:alpha:]]$/) and !['A','I','E','O','U'].include?(s[0].upcase)
+    return true
+  end
+  return false
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  if s.size==0 or s.count('01') != s.size
+    return false
+  end
+  
+  val = s.to_i(2)
+  if val%4==0
+    return true
+  end
+  return false
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  attr_accessor :price
+  attr_accessor :isbn
+  
+  def initialize(isbn, price)
+    if isbn.size ==0 or price <=0
+      raise ArgumentError, "In valid arguments"
+    end
+    @isbn = isbn
+    @price = price
+  end
+  
+  def price_as_string
+    return "$%.2f"%@price.to_f()
+  end
 end
